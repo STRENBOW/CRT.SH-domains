bd46566b87a0df9752e55e48a8498ef8ad1ede38
STRENBOW <90793240+STRENBOW@users.noreply.github.com>
2024-04-21 11:03:31 +0000

    Update scarping.py

--- scarping.py
+++ scarping.py
@@ -11,19 +11,30 @@ def extract_domains(text):
 
 def main ():
     if len(sys.argv) > 1:
-        domain = sys.argv[1]  # Use the command-line argument as the domain
+        domain = sys.argv[1]
+        with requests.session() as s: 
+            req = s.get(url=f"https://crt.sh/?q={domain}",headers=user)
+            source = req.content
+            html = HTML(html=source)
+            subdomains = html.text
+            domain_names = extract_domains(subdomains)
+            for dom in domain_names:
+                if domain in dom and ("*." not in dom) and (dom not in list) and ("Type: Identity    Match: ILIKE    Search:" not in dom) and ("crt.sh |" not in dom):
+                    print(dom)
+                    list.append(dom)
     else:
-        domain = sys.stdin.read().strip()  # Read the domain from stdin if no argument is provided
-    with requests.session() as s:
-        req = s.get(url=f"https://crt.sh/?q={domain}",headers=user)
-        source = req.content
-        html = HTML(html=source)
-        subdomains = html.text
-        domain_names = extract_domains(subdomains)
-        for dom in domain_names:
-            if domain in dom and ("*." not in dom) and (dom not in list) and ("Type: Identity    Match: ILIKE    Search:" not in dom) and ("crt.sh |" not in dom):
-                print(dom)
-                list.append(dom)
-
+        domains = sys.stdin.readlines()
+        with requests.session() as s: 
+            for domain in domains :
+                domain = domain.strip()
+                req = s.get(url=f"https://crt.sh/?q={domain}",headers=user)
+                source = req.content
+                html = HTML(html=source)
+                subdomains = html.text
+                domain_names = extract_domains(subdomains)
+                for dom in domain_names:
+                    if domain in dom and ("*." not in dom) and (dom not in list) and ("Type: Identity    Match: ILIKE    Search:" not in dom) and ("crt.sh |" not in dom):
+                        print(dom)
+                        list.append(dom)
 if __name__ == "__main__":
     main()
